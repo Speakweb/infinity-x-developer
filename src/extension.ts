4711cd08dbfa5e9afc6475dad105cc96ffab3a19
brendanshen24 <brendanshen@hotmail.com>
2023-05-28 15:18:57 -0700

    implemented marvin's changes, now the user is given a textbox where they can request a change

--- src/extension.ts
+++ src/extension.ts
@@ -2,30 +2,31 @@ import * as vscode from 'vscode';
 import { getChatGPTResponse } from './gpt';
 
 export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('extension.showSelectedText', () => {
-        const editor = vscode.window.activeTextEditor;
-        if (editor) {
-            const selectedText = editor.document.getText(editor.selection);
-            async function main() {
-                try {
-                  const prompt = 'Give me a git diff of changes needed for this code: ' + selectedText;
-                  showSelectedText(prompt);
-                  const response = await getChatGPTResponse(prompt);
-                  //console.log(response);
-                  showSelectedText(response);
-                } catch (error) {
-                  console.error(error);
-                }
-              }
-              main();
-        }
-    });
+  let disposable = vscode.commands.registerCommand('extension.showSelectedText', () => {
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+      const selectedText = editor.document.getText(editor.selection);
+      const inputBoxOptions: vscode.InputBoxOptions = {
+        prompt: 'Enter your request',
+        //value: selectedText,
+      };
 
-    context.subscriptions.push(disposable);
+      vscode.window.showInputBox(inputBoxOptions).then(async (userInput) => {
+        if (userInput) {
+          const prompt =  'Give me a git diff of the changes needed for the following request and code: ' + userInput + '. Here is the existing code: \n\n' + selectedText;
+          showSelectedText(prompt);
+          const response = await getChatGPTResponse(prompt);
+          showSelectedText(response);
+        }
+      });
+    }
+  });
+
+  context.subscriptions.push(disposable);
 }
 
 function showSelectedText(selectedText: string) {
-    vscode.window.showInformationMessage(selectedText);
+  vscode.window.showInformationMessage(selectedText);
 }
 
 export function deactivate() {}
