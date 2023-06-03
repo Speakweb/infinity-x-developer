bf882adfb2c36fb7ded87a212e9374f5b160928e
Anton-Pan <anton.v.panfilenok@gmail.com>
2023-06-02 18:59:06 -0700

    added a separate window that opens with the gpt response

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,7 @@
 import * as vscode from 'vscode';
 import { getChatGPTResponse } from './gpt';
+import { TaskPanelKind } from 'vscode';
+import { extensions } from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand('extension.showSelectedText', () => {
@@ -7,26 +9,84 @@ export function activate(context: vscode.ExtensionContext) {
     if (editor) {
       const selectedText = editor.document.getText(editor.selection);
       const inputBoxOptions: vscode.InputBoxOptions = {
-        prompt: 'Enter your request',
-        //value: selectedText,
+        prompt: 'Enter your request'
       };
 
       vscode.window.showInputBox(inputBoxOptions).then(async (userInput) => {
         if (userInput) {
-          const prompt =  'Give me a git diff of the changes needed for the following request and code: ' + userInput + '. Here is the existing code: \n\n' + selectedText;
+          const prompt =  `Give me a git patch of the changes needed for the following request and code.  Return only a valid and parsable git patch with no extra text. 
+          Request: ${userInput}
+          Code: ${selectedText}`;
           showSelectedText(prompt);
           const response = await getChatGPTResponse(prompt);
-          showSelectedText(response);
+          openPopupTextWindow(response, selectedText);
         }
+        //todo turn into function thenc all from botton
       });
     }
   });
-
   context.subscriptions.push(disposable);
 }
 
 function showSelectedText(selectedText: string) {
   vscode.window.showInformationMessage(selectedText);
+}
+
+function openPopupTextWindow(GPTreply: string, selectedText: string) {
+  const panel = vscode.window.createWebviewPanel(
+      'popupTextWindow',
+      'GPT Response',
+      vscode.ViewColumn.One,
+      {
+        enableScripts: true,
+        retainContextWhenHidden: true
+      }
+  );
+
+  panel.webview.html = `
+    <html>
+    <body>
+      <div>
+        <p>${GPTreply}</p>
+        <div class="btn-group">
+          <br>
+          <button onclick="applyChanges()">Apply Changes</button>
+          <button onclick="tryAgain()">Try Again With a Different Prompt</button>
+          <button onclick="closePanel()">Close</button>
+        </div>
+      </div>
+      
+      <script>
+        const vscode = acquireVsCodeApi();
+        
+        function applyChanges() {
+          vscode.postMessage({ command: 'applyChanges' });
+        }
+        
+        function tryAgain() {
+          vscode.postMessage({ command: 'tryAgain' });
+        }
+        
+        function closePanel() {
+          vscode.postMessage({ command: 'closePanel' });
+        }
+      </script>
+    </body>
+    </html>
+  `;
+
+  panel.webview.onDidReceiveMessage(message => {
+    let command = message.command;
+    if (command === 'applyChanges') {
+      // do stuff
+    }
+    else if (command === 'tryAgain') {
+      // call the show input box function again
+    }
+    else if (command === 'closePanel') {
+      panel.dispose();
+    }
+  });
 }
 
 export function deactivate() {}
