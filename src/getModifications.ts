a991798301f1cd6cb5668bcfa2cbe98104948701
marvinirwin <marvin@marvinirwin.com>
2023-06-16 15:01:30 -0700

    added writing function, didn't test or use

--- src/getModifications.ts
+++ src/getModifications.ts
@@ -1,5 +1,5 @@
-
-export type FilePath = string;
+import * as fs from "fs";
+import * as vscode from "vscode";
 
 export interface Content {
     startLine: number;
@@ -8,12 +8,44 @@ export interface Content {
 }
 
 export interface RelevantFile {
-    path: FilePath;
+    path: string;
     content: Content[];
 }
+export type Output = RelevantFile[];
 
 export type Input = RelevantFile[];
-export type Output = RelevantFile[];
+export const editFiles = async (output: Output): Promise<void> => {
+    const textEditorMap: Map<string, vscode.TextEditor> = new Map();
+
+    for (const file of output) {
+        if (!fs.existsSync(file.path)) {
+            throw new Error(`File not found: ${file.path}`);
+        }
+
+        let editor: vscode.TextEditor | undefined = textEditorMap.get(file.path);
+
+        if (!editor) {
+            const uri = vscode.Uri.file(file.path);
+            const document = await vscode.workspace.openTextDocument(uri);
+            editor = await vscode.window.showTextDocument(document);
+            textEditorMap.set(file.path, editor);
+        }
+
+        const edit = new vscode.WorkspaceEdit();
+
+        for (const content of file.content) {
+            const start = new vscode.Position(content.startLine, 0);
+            const end = new vscode.Position(content.startLine + content.contents.length, 0);
+
+            const linesToReplace = new vscode.Range(start, end);
+            const newText = content.contents.join('\n');
+
+            edit.replace(editor.document.uri, linesToReplace, newText);
+        }
+
+        await vscode.workspace.applyEdit(edit);
+    }
+};
 
 export const getModificationsPrompt = ({files, request}: { files: RelevantFile[], request: string }) => `
 You are an excellent developer who follows best coding practices.  You will receive input in the shapes described below and then will call the "modifyCode" function with your modifications.
